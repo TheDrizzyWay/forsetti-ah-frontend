dcff42882d54980ae2094a04c208f1a21e2efc78
Christopher Iliyas <christopheriliyas@gmail.com>
2019-11-29 00:33:57 +0100

    add backend live url

--- src/config/axiosConfig.js
+++ src/config/axiosConfig.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 // https://drizzyforsettibackend.herokuapp.com/api/v1
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api/v1'
+  baseURL: 'https://drizzyforsettibackend.herokuapp.com/api/v1'
 });
 
 export default instance;
